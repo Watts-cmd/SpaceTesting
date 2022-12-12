24bda28bbb175d48473d9f7a5ce4e0d55977e40e
Watts <Justinjwatts@outlook.com>
2022-12-12 13:06:31 -0500

    Version 2.0 A new Beginning

--- spaceGame/src/game/SpaceMain.java
+++ spaceGame/src/game/SpaceMain.java
@@ -5,13 +5,8 @@ package game;
 public class SpaceMain {
 
 	public static void main(String[] args) {
-		System.out.println("This Will be the main game");
-
-		System.out.println("New Branch?");
-		
-		System.out.println("SO this should now be on main but can I add this and save it on the enw branch?");
-		
-		System.out.println("More work?");
+		//Update 2.0
+		//Simplyfiying Everything by deleteing everything
 	}
 
 }
