596f33c24ca0532fdfed3c3dc396a938a23dcaf0
Watts <Justinjwatts@outlook.com>
2022-12-12 13:21:46 -0500

    V 2.1 lets try

--- spaceGame/src/game/SpaceMain.java
+++ spaceGame/src/game/SpaceMain.java
@@ -2,11 +2,13 @@ package game;
 //Justin Watts ~ Dec. 12, 2022
 //Trying to Learn git
 
+//V2.1
+//Lets give it a try
+
 public class SpaceMain {
 
 	public static void main(String[] args) {
-		//Update 2.0
-		//Simplyfiying Everything by deleteing everything
+		//WEeeee
 	}
 
 }
