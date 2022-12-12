5ff99bcbb597aee4f3e7d7058ccc86593d33e2bf
Watts <Justinjwatts@outlook.com>
2022-12-12 12:26:05 -0500

    Seeing how making a new Branch Works

---
+++ spaceGame/src/game/SpaceMain.java
@@ -0,0 +1,13 @@
+package game;
+//Justin Watts ~ Dec. 12, 2022
+//Trying to Learn git
+
+public class SpaceMain {
+
+	public static void main(String[] args) {
+		System.out.println("This Will be the main game");
+
+		System.out.println("New Branch?");
+	}
+
+}
