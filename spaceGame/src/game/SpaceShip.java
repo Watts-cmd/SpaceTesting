c3f605bdeb518166dd06eb566cd41b0ad4932f61
Watts <Justinjwatts@outlook.com>
2022-12-13 12:04:51 -0500

    v 2.2 made SpaceShip object

---
+++ spaceGame/src/game/SpaceShip.java
@@ -0,0 +1,15 @@
+package game;
+
+import java.awt.Rectangle;
+
+//Justin Watts ~ Dec. 13, 2022
+
+public class SpaceShip extends Rectangle {
+	int x,y;
+	int speed;
+
+	SpaceShip(int x, int y){
+	 this.x = x;
+	 this.y = y;
+	}
+}
